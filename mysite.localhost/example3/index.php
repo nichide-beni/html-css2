2f72bb2d9308b91a2452ad1b0748ee0e042f70dc
beni <benny.software.developer@gmail.com>
2025-10-08 16:12:27 +0900

    hotfix: echoの漏れを修正

--- mysite.localhost/example3/index.php
+++ mysite.localhost/example3/index.php
@@ -51,28 +51,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <label>お名前：
       <input type="text" name="name" value="<?php htmlspecialchars($name) ?>">
       <?php if (isset($errors['name'])): ?>
-        <div class="error"><?php $errors['name'] ?></div>
+        <div class="error"><?php echo $errors['name'] ?></div>
       <?php endif; ?>
     </label>
 
     <label>メールアドレス：
       <input type="email" name="email" value="<?php htmlspecialchars($email) ?>">
       <?php if (isset($errors['email'])): ?>
-        <div class="error"><?php $errors['email'] ?></div>
+        <div class="error"><?php echo $errors['email'] ?></div>
       <?php endif; ?>
     </label>
 
     <label>パスワード：
       <input type="password" name="password" value="">
       <?php if (isset($errors['password'])): ?>
-        <div class="error"><?php $errors['password'] ?></div>
+        <div class="error"><?php echo $errors['password'] ?></div>
       <?php endif; ?>
     </label>
 
     <label>お問い合わせ内容：
       <textarea name="message" rows="5"><?php nl2br(htmlspecialchars($message, ENT_QUOTES, 'UTF-8')) ?></textarea>
       <?php if (isset($errors['message'])): ?>
-        <div class="error"><?php $errors['message'] ?></div>
+        <div class="error"><?php echo $errors['message'] ?></div>
       <?php endif; ?>
     </label>
 
